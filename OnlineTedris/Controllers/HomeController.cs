ca9efbafff7ecbe0d7cf82cf8ddf029f2e7d20c1
seddam <bseddam@gmail.com>
2019-01-24 18:00:20 +0300

    new project

---
+++ OnlineTedris/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using OnlineTedris.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace OnlineTedris.Controllers
+{
+    public class HomeController : Controller
+    {
+        DbTraining db = new DbTraining();
+        public ActionResult Index()
+        {
+            return View(db.Telebes.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ OnlineTedris/Models/DbModels/Telebe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTedris.Models.DbModels
+{
+    public class Telebe
+    {
+        public int ID { get; set; }
+        public string IstifadeciAdi { get; set; }
+        public string Shifre { get; set; }
+        public string Soyad { get; set; }
+        public string Adi { get; set; }
+        public string Ata_adi { get; set; }
+        public int Cins { get; set; }
+        public DateTime Tevellud { get; set; }
+        public string Shekil { get; set; }
+        public string TelebeKodu { get; set; }
+        public List<TelebeSebet> telebeSebets { get; set; }
+        public List<TelebeHesab> telebeHesabs { get; set; }
+        public List<Odenisfenn> odenisfenns { get; set; }
+        public List<TelebeninKursu> telebeninKursus { get; set; }
+    }
+}
